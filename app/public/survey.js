e8f106d9b454358515b7dea51d9f1216ffa0f7ea
Kyle Atchison <katchison@gmail.com>
2017-09-17 19:49:13 -0500

    fixed

--- app/public/survey.js
+++ app/public/survey.js
@@ -32,7 +32,7 @@ $("#submit").on("click", function (event) {
             var match = friendMatch(data);
             if (match.name !== undefined) {
                 modalTitle.text('You have a match!');
-                modalBody.html('<p class="strong">You\'re most compatible with:</p><p>' + match.name + '</p><img src="' + match.picURL + '" height="45">');
+                modalBody.html('<p class="strong">You\'re most compatible with:</p><p>' + match.name + '</p><img src="' + match.picURL + '" height="200">');
             } else {
                 modalTitle.text('Too few friends');
                 modalBody.text('Sorry, there are too few friends to match you with.');
@@ -68,7 +68,7 @@ function friendMatch(data) {
     });
     return {
         name: minName,
-        image: minImage
+        picURL: minImage
     };
 
 };
